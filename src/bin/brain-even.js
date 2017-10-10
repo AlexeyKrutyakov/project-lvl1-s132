c5d2c841f0ea2f351e498989d6e765a291ae4237
arctikbear <coder.arctik@gmail.com>
2017-10-10 17:34:25 +0300

    add step 4 in project

---
+++ src/bin/brain-even.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+import even from '..';
+
+console.log('Welcome to the Brain Games!\n' +
+            'Answer "yes" if number even otherwise answer "no".\n');
+even();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
 import readlineSync from 'readline-sync';
 
-export default () => {
+const greeting = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
+  return userName;
+};
+
+const correctAnswer = number => ((number % 2 === 0) ? 'yes' : 'no');
+
+const evenTestResult = (name, attempts) => {
+  if (attempts === 0) {
+    return `Congratulations, ${name}!`;
+  }
+  const number = Math.round(Math.random() * 100);
+  console.log(`Question: ${number}`);
+  const userAnswer = readlineSync.question('Your answer: ');
+  const correct = correctAnswer(number);
+  if (userAnswer === correct) {
+    console.log('Correct!');
+    return evenTestResult(name, attempts - 1);
+  }
+  return `${userAnswer} is wrong answer ;(. Correct answer was ${correct}.\nLet's try again, ${name}!`;
+};
+
+export default () => {
+  const userName = greeting();
+  const attempts = 3;
+  console.log(evenTestResult(userName, attempts));
 };
