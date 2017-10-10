3e2c09852e98a8603453e6ed2af0a95c0fa8f9a7
arctikbear <coder.arctik@gmail.com>
2017-10-10 14:51:19 +0300

    change require -> import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const readlineSync = require('readline-sync');
+import readlineSync from 'readline-sync';
 
 export default () => {
   const userName = readlineSync.question('May I have your name? ');
